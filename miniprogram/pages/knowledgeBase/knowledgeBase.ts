fc1307ceb88a8349031f41b09a9c1fd18fffc0b7
Lao_Liu <laoliu735@gmail.com>
2022-06-22 22:59:25 +0800

    Add: Knowledge Base Page
    with test data

--- miniprogram/pages/knowledgeBase/knowledgeBase.ts
+++ miniprogram/pages/knowledgeBase/knowledgeBase.ts
@@ -1,66 +1,23 @@
 // pages/knowledgeBase/knowledgeBase.ts
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
-
+        columnNames: [
+            "name1",
+            "name2",
+            "name3",
+            "name4",
+            "name4",
+            "name4",
+            "name4",
+            "name4",
+        ]
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
+    onClickColumn(event) {
+        let value = event.currentTarget.dataset.value;
+        let url = "column/column?column=" + value;
+        wx.navigateTo({
+            url: url,
+        });
     }
 })
