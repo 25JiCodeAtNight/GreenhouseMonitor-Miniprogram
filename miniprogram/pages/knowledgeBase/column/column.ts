037ef4d6618e394f4064e714f3f0962390a647f7
Lao_Liu <laoliu735@gmail.com>
2022-06-22 23:01:54 +0800

    Add: Knowledge Base Columns Page
    with test data

--- miniprogram/pages/knowledgeBase/column/column.ts
+++ miniprogram/pages/knowledgeBase/column/column.ts
@@ -1,66 +1,27 @@
 // pages/knowledgeBase/column/column.ts
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
-
+        "columnName": "",
+        "articles": [
+            { "articleTitle": "Title1", "articleIntro": "intro1intro1intro1intro1intro1intro1intro1intro1intro1" },
+            { "articleTitle": "Title2", "articleIntro": "intro2" },
+            { "articleTitle": "Title3", "articleIntro": "intro3" },
+            { "articleTitle": "Title4", "articleIntro": "intro4" },
+            { "articleTitle": "Title5", "articleIntro": "intro5" },
+        ]
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad() {
-
+    onLoad(option) {
+        this.setData({
+            "columnName": option.column
+        })
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
+    onClickArticle(event) {
+        let value = event.currentTarget.dataset.value;
+        let url = "article/article?artile=" + value;
+        wx.navigateTo({
+            url: url,
+        });
     }
 })
