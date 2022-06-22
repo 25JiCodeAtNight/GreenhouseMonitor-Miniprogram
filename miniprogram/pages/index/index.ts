e29fa7f591a4ea6d4eb88a246181f853466c279d
Lao_Liu <laoliu735@gmail.com>
2022-06-22 21:44:07 +0800

    Add: index page
    With test data

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -3,10 +3,50 @@
 const app = getApp<IAppOption>()
 
 Page({
-  data: {
-    
-  },
-  onLoad() {
-    
-  },
+    data: {
+        "cards": [
+            {
+                "greenhouseid": "123abc",
+                "name": "name1",
+                "temp": "26",
+                "humi": "80",
+                "stat": "正常",
+            },
+            {
+                "name": "name2",
+                "temp": "28",
+                "humi": "89",
+                "stat": "正常",
+            },
+            {
+                "name": "name2",
+                "temp": "28",
+                "humi": "89",
+                "stat": "正常",
+            },
+            {
+                "name": "name2",
+                "temp": "28",
+                "humi": "89",
+                "stat": "正常",
+            },
+
+            {
+                "name": "name2",
+                "temp": "28",
+                "humi": "89",
+                "stat": "正常",
+            },
+        ]
+    },
+    onLoad() {
+    },
+
+    onClickCard(event) {
+        let value = event.currentTarget.dataset.value;
+        let url = "detail/detail?greenhouseid=" + value;
+        wx.navigateTo({
+            url: url,
+        });
+    }
 })
