b3e7a20a9d46a96769c7f5e9c4b96b0271f3308f
Lao_Liu <laoliu735@gmail.com>
2022-06-24 03:37:24 +0800

    Update sensorManagement.ts

--- miniprogram/pages/aboutMe/sensorManagement/sensorManagement.ts
+++ miniprogram/pages/aboutMe/sensorManagement/sensorManagement.ts
@@ -1,12 +1,33 @@
 // pages/aboutMe/sensorManagement/sensorManagement.ts
 Page({
     data: {
-        numOfSensors: 5,
-        sensors: [
-            {"sensorName": "name1", "sensorID" : "123"},
-            {"sensorName": "name2", "sensorID" : "123"},
-            {"sensorName": "name3", "sensorID" : "123"},
-        ]
+        numOfSensors: 0,
+        sensors: [],
+    },
+
+    onLoad() {
+        const app = getApp();
+        let that = this;
+        const userID = wx.getStorageSync("userID");
+        let url = "http://" + app.globalData.serverAddress + "/v1/user/getAllSensors";
+        wx.request({
+            url: url,
+            header: {
+                'content-type': 'application/json'
+            },
+            data: {
+                "userID": userID,
+            },
+            success(res) {
+                that.setData({
+                    sensors: res.data["responds"],
+                })
+            }
+        })
+        let numOfSensors = this.data.sensors.length;
+        this.setData({
+            numOfSensors: numOfSensors,
+        })
     },
 
     addSensor() {
@@ -17,7 +38,22 @@ Page({
     },
 
     removeSensor(event) {
-        let sensorID = event.currentTarget.dataset.value;
-        let url = "";
+        const sensorID = event.currentTarget.dataset.value;
+        const app = getApp();
+        const userID = wx.getStorageSync("userID");
+        let that = this;
+        let url = "http://" + app.globalData.serverAddress + "/v1/sensor/cancel";
+        let data = {
+            "sensorID": sensorID,
+            "userID": userID,
+        }
+        wx.request({
+            url: url,
+            data: data,
+            method: "POST",
+            success(res) {
+                that.onLoad();
+            }
+        })
     }
 })
