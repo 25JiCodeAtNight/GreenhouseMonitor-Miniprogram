2f7d19f7a6c017c2745b1d53e7d3c8cb00388a84
Lao_Liu <laoliu735@gmail.com>
2022-06-23 23:40:16 +0800

    Add: Greenhouse Management Page

--- miniprogram/pages/aboutMe/greenhouseManagement/greenhouseManagement.ts
+++ miniprogram/pages/aboutMe/greenhouseManagement/greenhouseManagement.ts
@@ -1,66 +1,24 @@
 // pages/aboutMe/greenhouseManagement/greenhouseManagement.ts
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
-
+        numOfGreenhouses: 3,
+        greenhouses: [
+            {"greenhouseName": "name1", "greenhouseID" : "123"},
+            {"greenhouseName": "name2", "greenhouseID" : "123"},
+            {"greenhouseName": "name3", "greenhouseID" : "123"},
+        ]
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad() {
-
+    addGreenhouse() {
+        let url = "addGreenhouse/addGreenhouse"
+        wx.navigateTo({
+            url: url,
+        })
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
+    removeGreenhouse(event) {
+        let greenhouseID = event.currentTarget.dataset.value;
+        let url = "";
     }
+    
 })
