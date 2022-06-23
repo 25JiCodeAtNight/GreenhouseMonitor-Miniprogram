61306c7e6a51cedf7cb89e62adcc4f627e6f0df9
Lao_Liu <laoliu735@gmail.com>
2022-06-23 23:40:39 +0800

    Add: Sensor Management Page

--- miniprogram/pages/aboutMe/sensorManagement/sensorManagement.ts
+++ miniprogram/pages/aboutMe/sensorManagement/sensorManagement.ts
@@ -1,66 +1,23 @@
 // pages/aboutMe/sensorManagement/sensorManagement.ts
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
-
+        numOfSensors: 5,
+        sensors: [
+            {"sensorName": "name1", "sensorID" : "123"},
+            {"sensorName": "name2", "sensorID" : "123"},
+            {"sensorName": "name3", "sensorID" : "123"},
+        ]
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad() {
-
+    addSensor() {
+        let url = "addSensor/addSensor"
+        wx.navigateTo({
+            url: url,
+        })
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
+    removeSensor(event) {
+        let sensorID = event.currentTarget.dataset.value;
+        let url = "";
     }
 })
