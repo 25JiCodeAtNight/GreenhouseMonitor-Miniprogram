3ec437aecff0715f39f88c619e944543abec26bd
Lao_Liu <laoliu735@gmail.com>
2022-06-24 05:21:19 +0800

    Update detail.ts

--- miniprogram/pages/index/detail/detail.ts
+++ miniprogram/pages/index/detail/detail.ts
@@ -1,33 +1,49 @@
 // pages/index/detail/detail.ts
 Page({
     data: {
-        "greenhouseid": "123abc",
-        "greenhouseInfo": {
-            "name": "name",
-            "temp": 26.5,
-            "humi": 80.0,
-            "weather": "阴天",
-            "cloud": 80,
-            "windspeed": 7,
-            "pressure": 1006,
-        },
-        "sensorInfo": [
-            {
-                "time": "2022-06-22 13:58",
-                "temp": 37.5,
-                "humi": 12.0,
-            },
-            {
-                "time": "2022-06-22 13:59",
-                "temp": 37.4,
-                "humi": 12.0,
-            },
-        ]
+        "greenhouseid": "",
+        "greenhouseInfo": {},
+        "sensorInfo": [],
     },
 
     onLoad(option) {
+        let that = this;
+        const app = getApp();
         this.setData({
             "greenhouseid": option.greenhouseid,
+        })
+        // 获取大棚详细信息
+        let url = "http://" + app.globalData.serverAddress + "/v1/greenhouse/detail";
+        let greenhouseid = option.greenhouseid;
+        wx.request({
+            url: url,
+            header: {
+                'content-type': 'application/json'
+            },
+            data: {
+                "greenhouseid": greenhouseid,
+            },
+            success(res) {
+                that.setData({
+                    greenhouseInfo: res.data,
+                })
+            }
+        })
+        // 获得传感器数据
+        url = "http://" + app.globalData.serverAddress + "/v1/sensor/detail";
+        wx.request({
+            url: url,
+            header: {
+                'content-type': 'application/json'
+            },
+            data: {
+                "greenhouseid": greenhouseid,
+            },
+            success(res) {
+                that.setData({
+                    sensorInfo: res.data["details"],
+                })
+            }
         })
     },
 
