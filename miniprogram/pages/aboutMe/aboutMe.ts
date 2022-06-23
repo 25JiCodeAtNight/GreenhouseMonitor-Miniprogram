3a8489187d5a1dfe79f9122efe3f566026d11161
Lao_Liu <laoliu735@gmail.com>
2022-06-23 23:39:48 +0800

    Add: aboutMe page

--- miniprogram/pages/aboutMe/aboutMe.ts
+++ miniprogram/pages/aboutMe/aboutMe.ts
@@ -3,12 +3,19 @@ Page({
     data: {
         userInfo: {},
         hasUserInfo: false,
+        openID: "",
+        hasOpenID: false,
+    },
+    
+    onReady() {
     },
 
     onShow() {
         var hasUserInfo = wx.getStorageSync("hasUserInfo");
+        var hasOpenID = wx.getStorageSync("hasOpenID");
         this.setData({
-            hasUserInfo: hasUserInfo
+            hasUserInfo: hasUserInfo,
+            hasOpenID: hasOpenID
         })
         // Load User Profile
         if (this.data.hasUserInfo) {
@@ -21,9 +28,54 @@ Page({
                 }
             })
         }
+        // Load OpenID
+        if (this.data.hasOpenID) {
+            wx.getStorage({
+                key: "openID",
+                success: (res) => {
+                    this.setData({
+                        openID: res.data
+                    })
+                }
+            })
+        }
     },
 
-    getProfile() {
+    getOpenID() {
+        let that = this;
+        const app = getApp();
+        wx.login({
+            success (res) {
+              if (res.code) {
+                //发起网络请求
+                wx.request({
+                  url: "http://" + app.globalData.serverAddress + "/wechat/login?code=",
+                  data: {
+                    code: res.code
+                  },
+                  success: (res) => {
+                      that.setData({
+                          hasOpenID: true,
+                          openID: res.data.toString()
+                      })
+                      wx.setStorage({
+                          key: "hasOpenID",
+                          data: true
+                      })
+                      wx.setStorage({
+                          key: "openID",
+                          data: res.data.toString()
+                      })
+                  }
+                })
+              } else {
+                console.log('登录失败！' + res.errMsg)
+              }
+            }
+          })
+    },
+
+    register() {
         wx.getUserProfile({
             desc: '用于展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
             success: (res) => {
@@ -33,13 +85,22 @@ Page({
                 })
                 wx.setStorage({
                     key: "hasUserInfo",
-                    data: "true"
+                    data: true
                 })
                 wx.setStorage({
                     key: "userInfo",
                     data: res.userInfo
                 })
             }
+        })
+        // 连接后端注册 API
+    },
+
+    navigateTo(event) {
+        let value = event.currentTarget.dataset.value;
+        let url = value + "Management/" + value + "Management";
+        wx.navigateTo({
+            "url": url,
         })
     }
 })
